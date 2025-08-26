391c83d2f9bb75b7c189f40492770f062b8d6b1f
Techie Sam <blessingabolade15@gmail.com>
2025-08-26 22:39:32 +0100

    chore: project initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center border h-screen">
+      <Button>Hello</Button>
+    </div>
+  );
+}
